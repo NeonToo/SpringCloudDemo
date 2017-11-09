dc9d2e136a03640d10129c43437ee81b2483844d
NeonToo <469565714@qq.com>
2017-11-09 16:11:49 +0800

    eureka client enabled

--- src/main/java/com/spring/cloud/demo/web/controller/UserController.java
+++ src/main/java/com/spring/cloud/demo/web/controller/UserController.java
@@ -3,10 +3,14 @@ package com.spring.cloud.demo.web.controller;
 import com.spring.cloud.demo.persistence.model.User;
 import com.spring.cloud.demo.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -15,10 +19,18 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
         User user = userRepository.findOne(id);
 
         return user;
+    }
+
+    @GetMapping("/instances")
+    public List<ServiceInstance> getServiceInstances() {
+        return discoveryClient.getInstances("microservice-provider-user");
     }
 }
